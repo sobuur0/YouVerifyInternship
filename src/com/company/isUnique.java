ef07e6fa803057648d3bca2535c7c660ae89c9e2
t-bolt <isholasobuur@gmail.com>
2021-10-08 13:24:15 +0100

    (1) Implementation of the isUnique question

--- src/com/company/isUnique.java
+++ src/com/company/isUnique.java
@@ -1,12 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 class isUnique {
     public static boolean isCharactersUnique(String ch) {
-
+        for (int i = 1; i < ch.length(); i++) {
+            for (int j = 0; j < i; j++)
+                if (ch.charAt(i) == ch.charAt(j)) return false;
+        }
         return true;
     }
-
     public static void main(String[] args) {
-        
+        System.out.print("Enter a set of String: ");
+        Scanner checkString = new Scanner(System.in);
+        String st = checkString.nextLine();
+        if (isCharactersUnique(st)) {
+            System.out.println("Has all unique characters");
+        } else {
+            System.out.println("Does not all contain unique characters");
+        }
     }
 }
