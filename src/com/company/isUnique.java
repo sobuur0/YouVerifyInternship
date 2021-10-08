e61119a9cef6a2f5d8ac6196ee21a34ba844ee25
t-bolt <isholasobuur@gmail.com>
2021-10-08 12:46:40 +0100

    initial implementation of isUnique question

---
+++ src/com/company/isUnique.java
@@ -0,0 +1,12 @@
+package com.company;
+
+class isUnique {
+    public static boolean isCharactersUnique(String ch) {
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
