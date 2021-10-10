012f7ab28f7af7fdc0a68e3930e95a46fe457ee9
t-bolt <isholasobuur@gmail.com>
2021-10-10 22:52:06 +0100

    6 final implementation of the stringCompression question

--- src/com/company/stringCompression.java
+++ src/com/company/stringCompression.java
@@ -1,9 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 class stringCompression {
 
+    static String compress(String in) {
+        if (in.length() < 3) return in;
+
+        int n = in.length(), i, o = 0;
+        char[] out = new char[n + 1];
+        out[o++] = in.charAt(0);
+        out[o++] = '1';
+        for (i = 1; i < n && o < n; i++) {
+            if (in.charAt(i) == in.charAt(i - 1) && out[o - 1] != '9') {
+                // Continued streak
+                out[o - 1]++;
+            } else {
+                // New streak
+                out[o++] = in.charAt(i);
+                out[o++] = '1';
+            }
+        }
+        return (i == n && o < n) ? new String(out, 0, o) : in;
+    }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String st = input.nextLine();
 
+        System.out.println(compress(st));
     }
 }
